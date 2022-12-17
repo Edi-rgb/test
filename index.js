40c18dbee1c501f1ba9369d5f8e7153e6f73a59d
Zyppon <88236849+Zyppon@users.noreply.github.com>
2022-12-17 02:05:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const navLinks = document.querySelector('#navLinks');
+const navToggleBtn = document.querySelector('#navToggleBtn');
+
+navToggleBtn.addEventListener('click',()=> {
+  navLinks.classList.toggle('active');
+});
+
+  
+
+
+
+
+
+ 
+
+
+
+
